67fa253283e05e8a94663b6cdf0b9e637cff797f
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-02-20 21:38:40 +0200

    Update BookList.jsx

--- src/components/Statistics/BookList/BookList.jsx
+++ src/components/Statistics/BookList/BookList.jsx
@@ -1,21 +1,18 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-export default function BookList({ data }) {
+export default function BookList({data}) {
   const {
     label,
     percentage } = data
-  console.log(label);
     return <li className="item" >
       <span className="label">{label}</span>
       <span className="percentage">{percentage}%</span>
     </li>
 
 } 
-// BookList.propTypes = PropTypes.arrayOf(PropTypes.shape({
-//    data: PropTypes.shape({
-//             // label: PropTypes.string.isRequired,
-//             // percentage: PropTypes.number.isRequired
-//         })
-//   // data:
-//   // label: PropTypes.string.isRequired
-// }))
+BookList.propTypes = { 
+  data: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired
+  })
+} 
